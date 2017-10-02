d50700d92c701a2fd48dbcb3ac286ddf736f4c30
Otavio Augusto <tavioalves@gmail.com>
2017-10-02 15:51:31 -0300

    Geração aleatoria de cores e inicil de fluxo de dados gerados

--- simon.js
+++ simon.js
@@ -1,8 +1,12 @@
-$(document).ready(function(){
+var	sequence = [];
+var chooseSequence = [];
+var simonOn = false;
+var gameStarted = false;
 
-	var simonOn = false;
+$(document).ready(function() {	
 	
-	$("#switch-slot").click(function(){
+	
+	$("#switch-slot").click(function() {
 		var switchValue = $("#switch")[0].className;
 		var countValue = $("#screen-info");
 
@@ -17,17 +21,15 @@ $(document).ready(function(){
 		}
 	});
 
-	$("#button-start").click(function(){
-		
+	$("#button-start").click(function() {
 		if(simonOn){
-			console.log("Starting the game...");
+			startGame();
 		} else {
 			console.log("Please start the Simon!");
 		}
-
 	});
 
-	$("#button-strict").click(function(){
+	$("#button-strict").click(function() {
 		var ledStrict = $("#led-strict")[0].className;
 
 		if(simonOn){
@@ -39,16 +41,36 @@ $(document).ready(function(){
 		} else {
 			console.log("Please start the Simon!");
 		}
-			
 	});
 });
 
-function startGame(){
-	var sequence = [];
-	var choose
+function startGame() {
+	sequence = [];
+	chooseSequence = [];
+
+	gameStarted = true;
+	runningGame();
+	
 }
 
-function chooseOption(){
+function chooseOption(value) {
+	if(simonOn && gameStarted) {
+		console.log("The button of color", value, "was clicked!");
+	} else {
+		console.log("Please turn on the Simon and start the game!");
+	}
+}
 
+function chooseNextColor() {
+	var colors = ["Yellow", "Blue", "Red", "Green"];
+	var option =  Math.floor((Math.random() * 3) +1);
+	var randomOption = colors[option];
+	
+	sequence.push(randomOption);
+}
+
+function runningGame() {
+	chooseNextColor();
+	console.log("Sequence now is: " + sequence);
 }
 
