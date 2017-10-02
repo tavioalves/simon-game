59e8a9bd09bd09aab1e5cf4e4f3dc9fef66bc6a7
Otavio Augusto <tavioalves@gmail.com>
2017-10-02 13:35:30 -0300

    Adição de novos estilos e sombras, e butão toggle para ligar ou desligar simon

--- simon.js
+++ simon.js
@@ -1,3 +1,14 @@
-function helloWorld() {
-  console.log("Fui clicado!\n");
-}
+$(document).ready(function(){
+	$("#switch-slot").click(function(){
+		var switchValue = $("#switch")[0].className;
+		var countValue = $("#screen-info");
+
+		if(switchValue == "") {
+			$("#switch").addClass("switch-on");
+			countValue.text("--");
+		} else {
+			$("#switch").removeClass("switch-on");
+			countValue.text("");
+		}
+	});
+});
