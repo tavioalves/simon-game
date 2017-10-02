049d775b6b9f859a01cbecbc59d96dc326ab3ea3
Otavio Augusto <tavioalves@gmail.com>
2017-10-02 15:22:40 -0300

    Inicialização de funcionalidades do simon game

--- simon.js
+++ simon.js
@@ -1,26 +1,54 @@
 $(document).ready(function(){
+
+	var simonOn = false;
 	
 	$("#switch-slot").click(function(){
 		var switchValue = $("#switch")[0].className;
 		var countValue = $("#screen-info");
 
 		if(switchValue == "") {
+			simonOn = true;
 			$("#switch").addClass("switch-on");
 			countValue.text("--");
 		} else {
+			simonOn = false;
 			$("#switch").removeClass("switch-on");
 			countValue.text("");
 		}
 	});
 
+	$("#button-start").click(function(){
+		
+		if(simonOn){
+			console.log("Starting the game...");
+		} else {
+			console.log("Please start the Simon!");
+		}
+
+	});
+
 	$("#button-strict").click(function(){
 		var ledStrict = $("#led-strict")[0].className;
 
-		if(ledStrict == "") {
-			$("#led-strict").addClass("led-on");
+		if(simonOn){
+			if(ledStrict == "") {
+				$("#led-strict").addClass("led-on");
+			} else {
+				$("#led-strict").removeClass("led-on");
+			}
 		} else {
-			$("#led-strict").removeClass("led-on");
+			console.log("Please start the Simon!");
 		}
+			
 	});
-
 });
+
+function startGame(){
+	var sequence = [];
+	var choose
+}
+
+function chooseOption(){
+
+}
+
